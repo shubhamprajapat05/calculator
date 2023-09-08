b18a5e7f29d4bbe3dffafe647b65734fd71c02fb
shubhamprajapat05 <110269934+shubhamprajapat05@users.noreply.github.com>
2023-09-08 09:12:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let result = document.getElementById("result");
 let currentInput = "";
+let historyList = document.getElementById("history-list");
 
 function appendToResult(value) {
     currentInput += value;
@@ -13,12 +14,27 @@ function clearResult() {
 
 function calculateResult() {
     try {
+        const expression = currentInput;
         currentInput = eval(currentInput);
         result.value = currentInput;
+
+        // Store the calculation history
+        const historyItem = document.createElement('li');
+        historyItem.innerHTML = `
+            ${expression} = ${currentInput}
+            <button onclick="deleteHistoryItem(this)">Clear</button>
+        `;
+        historyList.appendChild(historyItem);
+
     } catch (error) {
         result.value = "Error";
         currentInput = "";
     }
+}
+
+function deleteHistoryItem(button) {
+    const listItem = button.parentNode;
+    historyList.removeChild(listItem);
 }
 
 function backspace() {
@@ -33,7 +49,7 @@ function handleKeyPress(key) {
         clearResult();
     } else if (key === "Backspace") {
         backspace();
-    } else if (key === "Delete") { // Added handling for "Delete" key
+    } else if (key === "Delete") {
         clearResult();
     } else if (!isNaN(key) || key === "." || "+-*/()".includes(key)) {
         appendToResult(key);
